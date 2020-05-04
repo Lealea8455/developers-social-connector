71432b3d09305f622b9bd3aa3b0d4b2fd826fb6b
Lealea8455 <lealea8455@gmail.com>
2020-05-04 16:13:26 +0300

    Set up basic api routes (users, auth, profile, posts)

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-//connect to MongoDB
+// Connect to MongoDB
 connectDB();
 
-app.get('/', (req, res) => {
-  res.send('API running');
-});
+app.get('/', (req, res) => { res.send('API running'); });
+
+// Define routes
+const userRoute = require('./routes/api/users');
+const authRoute = require('./routes/api/auth');
+const profileRoute = require('./routes/api/profile');
+const postsRoute = require('./routes/api/posts');
+
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/profile', profileRoute);
+app.use('/api/posts', postsRoute);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
