400c108e61e0a1b3c3808ab114aa2dc7c5edeb70
Lealea8455 <lealea8455@gmail.com>
2020-05-04 15:47:17 +0300

    create db config file and connect to MongoDB

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
-
-const app = express();
+const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
+const app = express();
+
+//connect to MongoDB
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('API running');
