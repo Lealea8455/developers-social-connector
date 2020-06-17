2937ac5b0a6f5d592967bdd45704e6d9680e35a8
Lealea8455 <lealea8455@gmail.com>
2020-06-17 17:47:14 +0300

    Handle Create&Edit profile
    - Add Create&Edit profile actions to actions/profile.js
    - Add dashboard options (Edit, Add experience, Add education) routing
    file
    - Add redux devtools configuration to trace action
    - Add edit profile form component

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -5,6 +5,7 @@ import {
   GET_PROFILE,
   PROFILE_ERROR
 } from './types';
+import { setAlert } from './alert';
 
 // Get current user's profile
 export const getCurrentProfile = () => async dispatch => {
@@ -22,3 +23,39 @@ export const getCurrentProfile = () => async dispatch => {
     });
   }
 };
+
+// Create or update profile 
+export const createProfile = (formDate, history, edit = false) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-type': 'application/json'
+      }
+    }
+
+    const res = await axios.post('/api/profile', formDate, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+
+    if (!edit) {
+      history.push('/dashboard');
+    }
+
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+}
