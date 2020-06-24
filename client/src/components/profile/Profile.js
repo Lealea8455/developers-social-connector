af3dae647ba41ccdf5d17fc561d0cc402f4b8bce
Lealea8455 <lealea8455@gmail.com>
2020-06-24 13:46:05 +0300

    Profile page
    - Add edit button if user is authenticated
    - Create ProfileTop component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,11 +4,12 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
+import ProfileTop from './ProfileTop';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  }, [getProfileById]);
+  }, [getProfileById, match.params.id]);
 
   return (
     <Fragment>
@@ -17,11 +18,18 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
           <Link to="/profiles" className="btn btn-black">
             Back To Profiles
           </Link>
-          {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id &&
-            (<Link to="/edit-profile" className="btn btn-dark">
-              Edit Profile
-            </Link>)
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id &&
+            (
+              <Link to="/edit-profile" className="btn btn-dark">
+                Edit Profile
+              </Link>
+            )
           }
+          <div className="profile-grid my-1">
+            <ProfileTop profile={profile} />
+          </div>
         </Fragment>}
     </Fragment>
   )
