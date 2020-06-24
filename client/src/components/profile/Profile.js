886644b5a4bd0ecf4948e0da293ee17b4e3a2a33
Lealea8455 <lealea8455@gmail.com>
2020-06-24 18:45:01 +0300

    Profile component
    - Create ProfileAbout component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,6 +5,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
 import ProfileTop from './ProfileTop';
+import ProfileAbout from './ProfileAbout';
 
 const Profile = ({ getProfileById, profile: { profile, loading }, auth, match }) => {
   useEffect(() => {
@@ -29,6 +30,7 @@ const Profile = ({ getProfileById, profile: { profile, loading }, auth, match })
           }
           <div className="profile-grid my-1">
             <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
           </div>
         </Fragment>}
     </Fragment>
