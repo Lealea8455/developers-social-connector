4277637239136a4166e64d1649dc33efc9cd6d4b
Lealea8455 <lealea8455@gmail.com>
2020-07-08 21:02:13 +0300

    Add Like&Unlike functionality

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -16,4 +16,5 @@ export const UPDATE_PROFILE = 'UPDATE_PROFILE';
 export const ACCOUNT_DELETED = 'ACCOUNT_DELETED';
 export const GET_POSTS = 'GET_POSTS';
 export const POST_ERRORS = 'POST_ERRORS';
+export const UPDATE_LIKES = 'UPDATE_LIKES';
 
