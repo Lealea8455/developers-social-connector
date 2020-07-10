519fe2f39856de4698a18cde5b8d3f1fc5aef67f
Lealea8455 <lealea8455@gmail.com>
2020-07-10 20:42:13 +0300

    Fix
    - Add profile prop to react hook dependency array
    - Fix key in div (ProfileGithub)

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -39,7 +39,7 @@ const EditProfile = ({ profile: { profile, loading }, createProfile, getCurrentP
       youtube: loading || !profile.social ? '' : profile.youtube,
       instagram: loading || !profile.social ? '' : profile.instagram
     });
-  }, [loading, getCurrentProfile]);
+  }, [loading, getCurrentProfile, profile]);
 
   const {
     company,
