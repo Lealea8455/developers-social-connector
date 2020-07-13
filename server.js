24bfe4d7800391b72e36b98adf44f1aaff7e85fd
Lealea8455 <lealea8455@gmail.com>
2020-07-13 19:11:48 +0300

    Prepared for deploymend

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -11,8 +12,6 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => { res.send('API running'); });
-
 // Define routes
 const userRoute = require('./routes/api/users');
 const authRoute = require('./routes/api/auth');
@@ -23,5 +22,15 @@ app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/profile', profileRoute);
 app.use('/api/posts', postsRoute);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static files
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
